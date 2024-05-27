e4d78daff57f055feece048c7cddd70e05cf197e
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:37:35 +0700

    membuat prosedur XY akses public

--- abstraction/abstraction.cpp
+++ abstraction/abstraction.cpp
@@ -4,6 +4,13 @@ using namespace std;
 class abstracktIklas {
 private: string x, y;
 
+public:
+    //method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
 
 };
 
