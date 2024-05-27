4ca8cdbd5734a2543759e1abbc385f7f5a779441
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:46:09 +0700

    umur dibuat public

--- multipewaris/multipewaris.cpp
+++ multipewaris/multipewaris.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 class orang {
+public:
+    int Umur;
 
+    orang(int pUmur) :
+        Umur(pUmur)
+    {
+        cout << "Orang dibuta dengan umur " << Umur << "\n" << endl;
+    }
 };
+
 
 int main()
 {
