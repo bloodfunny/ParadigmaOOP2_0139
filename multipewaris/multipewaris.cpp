da969d68d734bc9d4948407ad6e03459cbfd93fa
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:53:53 +0700

    membuat class budi

--- multipewaris/multipewaris.cpp
+++ multipewaris/multipewaris.cpp
@@ -32,7 +32,16 @@ public:
     }
 };
 
+class budi : public pekerja, public pelajar {
+public:
 
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
 
 
 
