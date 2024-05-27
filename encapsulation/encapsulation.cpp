7f6c9efd454e2d9a8fbde2bec5fdbc19c38dc582
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:15:54 +0700

    void saklar dibuat public

--- encapsulation/encapsulation.cpp
+++ encapsulation/encapsulation.cpp
@@ -4,8 +4,16 @@ using namespace std;
 class remoteLampu {
 private:
     string saklarno[10];
-
+public:
+    void saklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
 };
+
+
 
 int main()
 {
