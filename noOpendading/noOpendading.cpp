bb5fa65865bd15376b4bf55fb6706db31bafb933
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:30:39 +0700

    menampilkan perkenalan dari class derived pada base class

--- noOpendading/noOpendading.cpp
+++ noOpendading/noOpendading.cpp
@@ -8,6 +8,12 @@ public:
     }
 };
 
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya function dari derived class ";
+    }
+};
 
 
 int main()
