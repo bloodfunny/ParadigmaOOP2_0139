fcb56b3fcb63835c1353f6928e6e67eeb32ad7e3
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-27 10:39:34 +0700

    menampilkan nilai

--- abstraction/abstraction.cpp
+++ abstraction/abstraction.cpp
@@ -11,7 +11,12 @@ public:
         x = a;
         y = b;
     }
+    //menampilkan nilai
+    void display() {
+        cout << "x -" << x << endl;
+        cout << "y = " << y << endl;
 
+    }
 };
 
 int main()
